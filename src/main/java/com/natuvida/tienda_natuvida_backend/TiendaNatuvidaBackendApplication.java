ba2f9b14f83422e26afa97137cd4a0eef0d72b7a
Melissa Diaz <120139230+melodiaz23@users.noreply.github.com>
2025-03-17 09:47:13 -0500

    Initialize project with Spring Boot and Maven setup

---
+++ src/main/java/com/natuvida/tienda_natuvida_backend/TiendaNatuvidaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.natuvida.tienda_natuvida_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaNatuvidaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaNatuvidaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natuvida/tienda_natuvida_backend/TiendaNatuvidaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natuvida.tienda_natuvida_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaNatuvidaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
