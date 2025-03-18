813cc5861058562ff41ba5743d545d271bf1268b
Melissa Diaz <120139230+melodiaz23@users.noreply.github.com>
2025-03-18 14:29:13 -0500

    Refactor Order, OrderItem, and Category entities to use UUID as the identifier

--- src/main/java/com/natuvida/store/entity/Order.java
+++ src/main/java/com/natuvida/store/entity/Order.java
@@ -18,7 +18,7 @@ import java.util.UUID;
 @Table(name = "orders")
 public class Order {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
   @NonNull
--- src/main/java/com/natuvida/store/entity/OrderItem.java
+++ src/main/java/com/natuvida/store/entity/OrderItem.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 public class OrderItem {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
   @ManyToOne
